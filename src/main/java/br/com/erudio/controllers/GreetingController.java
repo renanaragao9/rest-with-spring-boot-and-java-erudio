878c776e38d94ca98f114e0affb4e22bc9e7421c
renanaragao9 <renanaragao9@github.com>
2026-03-02 21:23:43 -0300

    [FTR] - Primeiro commoit

---
+++ src/main/java/br/com/erudio/controllers/GreetingController.java
@@ -0,0 +1,26 @@
+package br.com.erudio.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.model.Greeting;
+
+@RestController
+public class GreetingController {
+
+    private static final String TEMPLATE = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(
+        @RequestParam(value = "name", defaultValue = "World")
+        String name
+    ) {
+        return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name)); 
+    }
+}
+    
+
